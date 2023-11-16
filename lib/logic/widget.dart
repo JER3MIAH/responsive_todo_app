cd62a3e53c500c0f77312c6f4c4c9582bfff2eb9
JER3MIAH <jeremiahbrownie@gmail.com>
2023-11-17 00:40:14 +0100

    flutter codelabs layouts practice

--- lib/logic/widget.dart
+++ lib/logic/widget.dart
@@ -5,35 +5,55 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      textBaseline: TextBaseline.alphabetic,
-      children: [
-        Text(
-          'Hey!',
-          style: TextStyle(
-            fontSize: 30,
-            fontFamily: 'Futura',
-            color: Colors.blue,
+    var bodyLarge = Theme.of(context).textTheme.bodyLarge;
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          decoration: BoxDecoration(border: Border.all()),
+          width: MediaQuery.of(context).size.width * 0.9,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  const Icon(
+                    Icons.account_circle,
+                    size: 70,
+                  ),
+                  const SizedBox(width: 10),
+                  Column(
+                    children: [
+                      Text('Flutter McFlutter',
+                          style: Theme.of(context).textTheme.headlineMedium),
+                      Text('Experienced App Developer', style: bodyLarge),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('123 Main Street', style: bodyLarge),
+                  Text('(415) 555-0198', style: bodyLarge),
+                ],
+              ),
+              const SizedBox(height: 15),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.accessibility),
+                  Icon(Icons.timer),
+                  Icon(Icons.phone_android),
+                  Icon(Icons.phone_iphone),
+                ],
+              )
+            ],
           ),
         ),
-        Text(
-          'Hey!',
-          style: TextStyle(
-            fontSize: 50,
-            fontFamily: 'Futura',
-            color: Colors.green,
-          ),
-        ),
-        Text(
-          'Hey!',
-          style: TextStyle(
-            fontSize: 40,
-            fontFamily: 'Futura',
-            color: Colors.red,
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
